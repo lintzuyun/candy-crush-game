f0f3576d3745bd6286cb812c8bff0fbf9204bcc9
tzuyunlin <tzuyunlin.tw@gmail.com>
2021-12-02 11:27:33 +0000

    create game board with random color squares

---
+++ app.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const grid = document.querySelector(".grid");
+  const width = 8;
+  const squares = [];
+
+  const candyColors = ["red", "yellow", "orange", "purple", "green", "blue"];
+
+  // Create Board w/ random color squares
+  function createBoard() {
+    for (let i = 0; i < width * width; i++) {
+      const square = document.createElement("div");
+      let randomColor = Math.floor(Math.random() * candyColors.length);
+      square.style.backgroundColor = candyColors[randomColor];
+      grid.appendChild(square);
+      squares.push(square);
+    }
+  }
+  createBoard();
+});
