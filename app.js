738d0ac3aeac5c06c8d55669c9a80b335bd49057
tzuyunlin <tzuyunlin.tw@gmail.com>
2021-12-02 14:52:46 +0000

    add functions to check row and column for 3

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const grid = document.querySelector(".grid");
   const width = 8;
   const squares = [];
+  let score = 0;
 
   const candyColors = ["red", "yellow", "orange", "purple", "green", "blue"];
 
@@ -82,4 +83,58 @@ document.addEventListener("DOMContentLoaded", () => {
       squares[squareIdBeingDragged].style.backgroundColor = colorBeingDragged;
     }
   }
+
+  // Check for matches
+  // Check for row of Three
+  function checkRowForThree() {
+    for (i = 0; i < 61; i++) {
+      let rowOfThree = [i, i + 1, i + 2];
+      let decidedColor = squares[i].style.backgroundColor;
+      const isBlank = squares[i].style.backgroundColor === "";
+
+      const notValid = [6, 7, 14, 15, 22, 23, 30, 31, 38, 39, 46, 47, 54, 55];
+      if (notValid.includes(i)) continue;
+
+      if (
+        rowOfThree.every(
+          (index) =>
+            squares[index].style.backgroundColor === decidedColor && !isBlank
+        )
+      ) {
+        score += 3;
+        rowOfThree.forEach((index) => {
+          squares[index].style.backgroundColor = "";
+        });
+      }
+    }
+  }
+  checkRowForThree();
+
+  // Check for column of Three
+  function checkColumnForThree() {
+    // i <47 because the bottom 2 rows are not are valid for columns of 3
+    for (i = 0; i < 47; i++) {
+      let columOfThree = [i, i + width, i + width * 2];
+      let decidedColor = squares[i].style.backgroundColor;
+      const isBlank = squares[i].style.backgroundColor === "";
+      if (
+        columOfThree.every(
+          (index) =>
+            squares[index].style.backgroundColor === decidedColor && !isBlank
+        )
+      ) {
+        score += 3;
+        columOfThree.forEach((index) => {
+          squares[index].style.backgroundColor = "";
+        });
+      }
+    }
+  }
+  checkColumnForThree();
+
+  // The setInterval() method will continue calling the function until clearInterval() is called, or the window is closed.
+  window.setInterval(function () {
+    checkRowForThree();
+    checkColumnForThree();
+  }, 100);
 });
